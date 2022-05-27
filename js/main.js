0fdb0ba1efdd0a3bfa0a57038aa4f6d1ba3a89b8
Shion Takeno <takeno@laptop-takeno.ics.nitech.ac.jp>
2022-05-27 13:04:56 +0900

    add Google scholar link and fix the animation of menu bar

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function main() {
 
 (function () {
    'use strict';
-   
+
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -17,10 +17,10 @@ function main() {
         }
       });
 
-	
+
     // Show Menu on Book
     $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 500;
+        var navHeight = $(window).height() - 770;
         if ($(window).scrollTop() > navHeight) {
             $('.navbar-default').addClass('on');
         } else {
@@ -28,7 +28,7 @@ function main() {
         }
     });
 
-    $('body').scrollspy({ 
+    $('body').scrollspy({
         target: '.navbar-default',
         offset: 80
     });
@@ -41,7 +41,7 @@ function main() {
       $(".navbar-collapse").collapse('hide');
     }
   });
-	
+
   	// Portfolio isotope filter
     $(window).load(function() {
         var $container = $('.portfolio-items');
@@ -69,14 +69,14 @@ function main() {
         });
 
     });
-	
 
-    // Nivo Lightbox 
+
+    // Nivo Lightbox
     $('.portfolio-item a').nivoLightbox({
-            effect: 'slideDown',  
-            keyboardNav: true,                            
+            effect: 'slideDown',
+            keyboardNav: true,
         });
- 
+
 
 }());
 
