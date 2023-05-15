383d07b3a92dc407a8d915f8cc3a13dc17cecd96
takeno1995 <takeno.s.1995@gmail.com>
2023-05-15 12:47:55 +0900

    Modify the container size of navvar

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function main() {
 
     // Show Menu on Book
     $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 900;
+        var navHeight = $(window).height() - 940;
         if ($(window).scrollTop() > navHeight) {
             $('.navbar-default').addClass('on');
         } else {
